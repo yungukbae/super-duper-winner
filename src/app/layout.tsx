970e5d87cbb9a9f7a6ccbca702379151fe579e98
yungukbae <yungukbae@gmail.com>
2023-05-23 12:15:05 +0900

    Fefs

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import "./globals.css";
-import { Inter, Playfair_Display } from "next/font/google";
+import { Inter, Noto_Serif } from "next/font/google";
 
-const playfairDisplay = Playfair_Display({ subsets: ["latin"] });
+const font = Noto_Serif({ weight: "400", subsets: ["latin"] });
 
 export const metadata = {
   title: "Create Next App",
@@ -15,7 +15,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={playfairDisplay.className}>{children}</body>
+      <body className={font.className}>{children}</body>
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,13 +2,18 @@ import "./page.css";
 import Image from "next/image";
 
 export default function Home() {
+  const arr = [
+    1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+  ];
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-8">
+    <main className="flex flex-row gap-8 justify-center flex-wrap p-8">
       <div className="flip-card">
         <div className="flip-card-inner">
           <div className="flip-card-front">
-            <div className="w-full h-full bg-[#ffe012] p-8">
-              <span className="text-3xl">Aug</span>
+            <div className="flex justify-between w-full h-full flex-col bg-[#ffe012] p-4">
+              <span className="text-right text-2xl font-bold">17</span>
+              <span className="text-left text-4xl">Aug</span>
             </div>
           </div>
           <div className="flip-card-back">back</div>
